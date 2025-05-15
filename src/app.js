516310f1676fa62d988f39c73af437e020cf2d67
chadriley722 <chadriley722@gmail.com>
2025-05-14 18:35:18 -0600

    feat: add basic Express server with root route

---
+++ src/app.js
@@ -0,0 +1,18 @@
+// src/app.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('RV Journal API is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
