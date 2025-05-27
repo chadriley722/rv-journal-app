f2d88f535cae6b7cfaab6b6eee4df89a7ffcce3a
chadriley722 <chadriley722@gmail.com>
2025-05-27 09:06:50 -0600

    Remove all existing code in preparation for Ruby on Rails migration

--- backend/drizzle/0002_add_tow_vehicles_down.sql
+++
@@ -1,8 +0,0 @@
--- Drop tow_vehicles table
-DROP TABLE IF EXISTS "tow_vehicles";
-
--- Remove RV class from rvs table
-ALTER TABLE "rvs" DROP COLUMN IF EXISTS "class";
-
--- Drop RV class enum type
-DROP TYPE IF EXISTS "rv_class"; 
